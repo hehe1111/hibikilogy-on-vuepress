a9a08996701ddcfcf148f1057735cfae064db48b
hehe1111 <2908749709@qq.com>
2024-05-30 01:48:09 +0800

    feat: 标签列表页，每一篇的标题的右边，加上对应日期

--- packages/theme/src/client/components/TagPostList.vue
+++ packages/theme/src/client/components/TagPostList.vue
@@ -4,12 +4,19 @@
 
     <div v-for="(subItem, subIndex) in item.data" :key="subIndex" class="item">
       <RouterLink :to="subItem.path">
-        <p class="title">
-          {{ subItem.info.title }}
-        </p>
-        <p v-if="subItem.info.subtitle" class="subtitle">
-          {{ subItem.info.subtitle }}
-        </p>
+        <div class="item-flex">
+          <div>
+            <p class="title">
+              {{ subItem.info.title }}
+            </p>
+            <p v-if="subItem.info.subtitle" class="subtitle">
+              {{ subItem.info.subtitle }}
+            </p>
+          </div>
+          <p v-if="subItem.info.date">
+            {{ formateDateString(subItem.info.date) }}
+          </p>
+        </div>
       </RouterLink>
       <hr />
     </div>
@@ -19,6 +26,7 @@
 <script setup lang="ts">
 import type { PropType } from "vue";
 import type { GungnirThemePostData } from "../../shared";
+import { formateDateString } from "../utils";
 
 defineProps({
   data: {
